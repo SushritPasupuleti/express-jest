19a0a4465b5daa54738626b8087d2d31177d207f
Sushrit Pasupuleti <sushrit.pk21@gmail.com>
2021-07-12 11:43:55 +0530

    Update app.test.js

--- tests/app.test.js
+++ tests/app.test.js
@@ -1,2 +1,11 @@
 const request = require('supertest');
 const app = require('../index');
+
+describe('API Test', () => {
+    it('GET /', () => {
+        return request(app)
+        .get('/')
+        .expect('Content-Type', /json/)
+        .expect(200)
+})
+})
