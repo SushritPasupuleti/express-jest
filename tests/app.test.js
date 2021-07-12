3f74b74885146b32a77773096776b29d87cc387e
Sushrit Pasupuleti <sushrit.pk21@gmail.com>
2021-07-12 12:14:05 +0530

    fix async issues

--- tests/app.test.js
+++ tests/app.test.js
@@ -4,9 +4,21 @@ const app = require('../index');
 describe('API Test', () => {
     it('GET /', () => {
         return request(app)
-        .get('/')
-        .expect(200).then((res) => {
-            expect(res.body).toBeDefined();
-        })
-})
+            .get('/')
+            .expect(200).then((res) => {
+                expect(res.body).toBeDefined();
+            })
+    })
+
+    it('GET /hello-there', () => {
+        return request(app)
+            .get('/hello-there')
+            .expect(200).then((res) => {
+                expect(res.body).toBeDefined();
+            })
+    })
+
+    afterAll(done => {
+        app.close(done);
+      });
 })
